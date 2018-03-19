4f4355a1e971f69604b86dc5b32b82ef48570dff
Answer-D <bboy.answer.d@gmail.com>
2018-03-20 00:52:46 +0900

    20180319_2

--- gold_efficiency/models.py
+++ gold_efficiency/models.py
@@ -5,46 +5,77 @@ from django.db import models
 
 class Item(models.Model):
     """アイテムのモデル"""
-    # id = models.PositiveIntegerField(primary_key=True)
     riot_item_id = models.PositiveIntegerField()
     name = models.CharField()
-    # description = models.TextField()
-    # gold_json = models.CharField()
-    gold_base = models.PositiveIntegerField()
-    gold_purchasable = models.BooleanField()
-    gold_sell = models.PositiveIntegerField()
-    gold_total = models.PositiveIntegerField()
-    # stats_json = models.CharField()
-    # flat_armor_mod = models.DecimalField(max_digits=8, decimal_places=5)
-    # flat_crit_chance_mod = models.DecimalField(max_digits=8, decimal_places=5)
-    # flat_hp_pool_mod = models.DecimalField(max_digits=8, decimal_places=5)
-    # from_item_id = models.CommaSeparatedIntegerField()
-    # to_item_id = models.CommaSeparatedIntegerField()
+    base_cost = models.PositiveIntegerField()
+    total_cost = models.PositiveIntegerField()
+    is_purchasable = models.BooleanField()
+    sell_gold = models.PositiveIntegerField()
     image_json = models.CharField()
     maps_json = models.CharField()
-    tags_json = models.CharField()
-    version = models.CharField()
+    patch_version = models.CharField()
 
     def __str__(self):
         return self.name
 
 
 class Stats(models.Model):
-    """金銭価値のベース(1ADあたり○○Gとかそういう感じ)"""
-    name = models.CharField()
-    gold_value_base = models.FloatField()
+    """アイテムに紐づくステータスのモデル"""
+    BASE_VALUE = {
+        'FlatArmorMod': 0,
+        'FlatCritChanceMod': 0,
+        'FlatHPPoolMod': 0,
+        'FlatHPRegenMod': 0,
+        'FlatMPPoolMod': 0,
+        'FlatMagicDamageMod':0,
+        'FlatMovementSpeedMod': 0,
+        'FlatPhysicalDamageMod': 0,
+        'FlatSpellBlockMod': 0,
+        'PercentAttackSpeedMod': 0,
+        'PercentLifeStealMod': 0,
+        'PercentMovementSpeedMod': 0,
+    }
 
-
-class ItemStats(models.Model):
-    """ItemとStatsの紐づけモデル"""
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    stats = models.ForeignKey(Stats, on_delete=models.CASCADE)
-    stats_amount = models.FloatField()
+    name = models.CharField(choices=BASE_VALUE.keys())
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    def get_gold_value(self):
+        return self.BASE_VALUE[self.name] * self.amount
 
 
-class PassiveGoldValue(models.Model):
+class Effect(models.Model):
+    """アイテムに紐づく効果のモデル"""
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     description = models.CharField()
-    gold_value = models.FloatField()
+    is_evaluable = models.BooleanField()
+
+    def __str__(self):
+        return self.description
+
+    def get_gold_value(self):
+        if self.is_evaluable:
+            return eval(self.gold_value_rule)
+        else:
+            pass
 
 
+class Tag(models.Model):
+    """アイテムに紐づくタグのモデル"""
+    TAGS = {
+        'GoldPer', 'Damage', 'Health', 'Stealth', 'Consumable', 'Mana', 'Slow',
+        'NonbootsMovement', 'MagicPenetration', 'Armor', 'Tenacity', 'Boots',
+        'ArmorPenetration', 'Trinket', 'Aura', 'SpellDamage', 'Vision', 'Jungle',
+        'LifeSteal', 'Active', 'HealthRegen', 'AttackSpeed', 'Bilgewater', 'OnHit',
+        'SpellVamp', 'SpellBlock', 'CooldownReduction', 'Lane', 'CriticalStrike',
+        'ManaRegen'
+    }
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    tag = models.CharField(choices=TAGS, null=True)
+
+    def __str__(self):
+        return self.tag
