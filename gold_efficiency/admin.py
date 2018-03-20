4849151c4e49945e6a4f3f6def4cfd614c726e81
answer_d <bboy.answer.d@gmail.com>
2018-03-20 16:34:52 +0900

    20180320

--- gold_efficiency/admin.py
+++ gold_efficiency/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Item
+from .models import PatchVersion, Item, StatsMaster, Stats, Effect, TagMaster, Tag
 
 # Register your models here.
 
 
+admin.site.register(PatchVersion)
 admin.site.register(Item)
+admin.site.register(StatsMaster)
+admin.site.register(Stats)
+admin.site.register(Effect)
+admin.site.register(TagMaster)
+admin.site.register(Tag)
--- gold_efficiency/models.py
+++ gold_efficiency/models.py
@@ -3,25 +3,35 @@ from django.db import models
 # Create your models here.
 
 
+class PatchVersion(models.Model):
+    """パッチバージョン"""
+    version_str = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.version_str
+
+
 class Item(models.Model):
-    """アイテムのモデル"""
+    """アイテム"""
     riot_item_id = models.PositiveIntegerField()
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     base_cost = models.PositiveIntegerField()
     total_cost = models.PositiveIntegerField()
     is_purchasable = models.BooleanField()
     sell_gold = models.PositiveIntegerField()
-    image_json = models.CharField()
-    maps_json = models.CharField()
-    patch_version = models.CharField()
+    image_json = models.TextField()
+    from_item_str = models.CharField(max_length=50, null=True)
+    into_item_str = models.CharField(max_length=50, null=True)
+    depth = models.PositiveIntegerField(null=True)
+    patch_version = models.ForeignKey(PatchVersion, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class Stats(models.Model):
-    """アイテムに紐づくステータスのモデル"""
-    BASE_VALUE = {
+class StatsMaster(models.Model):
+    """ステータスと金銭価値のベース"""
+    STATS_BASE = {
         'FlatArmorMod': 0,
         'FlatCritChanceMod': 0,
         'FlatHPPoolMod': 0,
@@ -36,46 +46,56 @@ class Stats(models.Model):
         'PercentMovementSpeedMod': 0,
     }
 
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    name = models.CharField(choices=BASE_VALUE.keys())
-    amount = models.FloatField()
+    name = models.CharField(max_length=30)
+    gold_value_per_amount = models.FloatField()
+    patch_version = models.ForeignKey(PatchVersion, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-    def get_gold_value(self):
-        return self.BASE_VALUE[self.name] * self.amount
+
+class Stats(models.Model):
+    """アイテムに紐づくステータス"""
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    stats = models.ForeignKey(StatsMaster, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.item.name + '_' + self.stats.name
 
 
 class Effect(models.Model):
-    """アイテムに紐づく効果のモデル"""
+    """アイテムに紐づく効果(passive/active)"""
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    description = models.CharField()
+    description = models.CharField(max_length=200)
     is_evaluable = models.BooleanField()
 
     def __str__(self):
-        return self.description
+        return self.item.name + '_' + self.description
 
-    def get_gold_value(self):
-        if self.is_evaluable:
-            return eval(self.gold_value_rule)
-        else:
-            pass
+
+class TagMaster(models.Model):
+    """タグのベース"""
+    name = models.CharField(max_length=30)
+    patch_version = models.ForeignKey(PatchVersion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
-    """アイテムに紐づくタグのモデル"""
-    TAGS = {
-        'GoldPer', 'Damage', 'Health', 'Stealth', 'Consumable', 'Mana', 'Slow',
-        'NonbootsMovement', 'MagicPenetration', 'Armor', 'Tenacity', 'Boots',
-        'ArmorPenetration', 'Trinket', 'Aura', 'SpellDamage', 'Vision', 'Jungle',
-        'LifeSteal', 'Active', 'HealthRegen', 'AttackSpeed', 'Bilgewater', 'OnHit',
-        'SpellVamp', 'SpellBlock', 'CooldownReduction', 'Lane', 'CriticalStrike',
-        'ManaRegen'
-    }
+    """アイテムに紐づくタグ"""
+    # TAGS = {
+    #     'GoldPer', 'Damage', 'Health', 'Stealth', 'Consumable', 'Mana', 'Slow',
+    #     'NonbootsMovement', 'MagicPenetration', 'Armor', 'Tenacity', 'Boots',
+    #     'ArmorPenetration', 'Trinket', 'Aura', 'SpellDamage', 'Vision', 'Jungle',
+    #     'LifeSteal', 'Active', 'HealthRegen', 'AttackSpeed', 'Bilgewater', 'OnHit',
+    #     'SpellVamp', 'SpellBlock', 'CooldownReduction', 'Lane', 'CriticalStrike',
+    #     'ManaRegen'
+    # }
 
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    tag = models.CharField(choices=TAGS, null=True)
+    tag = models.ForeignKey(TagMaster, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.tag
+        return self.item.name + ':' + self.tag.name
