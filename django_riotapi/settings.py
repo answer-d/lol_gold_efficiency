3b576f59755b7d501c7f99e377a79b661250acb1
answer_d <bboy.answer.d@gmail.com>
2018-03-22 16:54:43 +0900

    bootstrapでview書き直した、サンプルデータの誤り修正、環境準備メモの誤り修正

--- django_riotapi/settings.py
+++ django_riotapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gold_efficiency',
+    'bootstrap4',  # django-bootstrap4
 ]
 
 MIDDLEWARE = [
--- gold_efficiency/views.py
+++ gold_efficiency/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Item, PatchVersion, Stats, Effect, STAB_TAGS, STAB_STATS_BASE
+from decimal import Decimal, ROUND_HALF_UP
 
 # Create your views here.
+
+
+def dec_round(v, fp):
+    """四捨五入マン"""
+    return Decimal(v).quantize(Decimal(fp), rounding=ROUND_HALF_UP)
 
 
 def index(request):
@@ -20,7 +26,7 @@ def index(request):
             stats_list.append({
                 'name': i.name,
                 'amount': i.amount,
-                'gold_value': STAB_STATS_BASE[i.name] * i.amount,
+                'gold_value': dec_round(STAB_STATS_BASE[i.name] * i.amount, '0.1'),
             })
             gold_value += STAB_STATS_BASE[i.name] * i.amount
 
@@ -30,19 +36,27 @@ def index(request):
                 'description': i.description,
                 'gold_value': None,
             })
+            # gold_value += TODO
 
-        tmp = {
+        # アイテムの価格が0Gの場合は金銭効率評価不可 → 0とする
+        try:
+            gold_efficiency = gold_value / item.total_cost
+        except ZeroDivisionError:
+            gold_efficiency = 0
+
+        elem = {
             'name': item.name,
             'total_cost': item.total_cost,
-            'gold_value': gold_value,
+            'gold_value': dec_round(gold_value, '0.1'),
             'stats': stats_list,
             'effects': effect_list,
-            'gold_efficiency': 100 * gold_value / item.total_cost,
+            'gold_efficiency': 100 * dec_round(gold_efficiency, '0.1'),
         }
 
-        item_list.append(tmp)
+        item_list.append(elem)
 
     context = {
+        'patch_version': patch_version,
         'item_list': item_list,
     }
 
