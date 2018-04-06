a71d6b9e36ce5e93c9706561795df2c1957f2211
Answer-D <bboy.answer.d@gmail.com>
2018-04-06 22:03:08 +0900

    アイテム詳細画面を作成、インプット入力して金銭価値を算出できるように。ついでに既存ページのURL変更

--- gold_efficiency/views.py
+++ gold_efficiency/views.py
@@ -1,21 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from .models import PatchVersion
+from .models import PatchVersion, Item
 from .backends import RiotStaticData
+
 
 # Create your views here.
 
 
 def index(request):
-    static_data = RiotStaticData()
-    json_path = "gold_efficiency/static/gold_efficiency/json/items_ver.8.6.1_na1.json"
-    items = static_data.load_from_json(json_path)
-    version = "8.6.1"
+    return render(request, 'gold_efficiency/index.html')
 
-    static_data.update_versions(version)
-    static_data.update_stats_base(items, version)
-    static_data.update_items(items, version)
 
-    patch_version = PatchVersion.objects.get(version_str=version)
+def itemlist(request):
+    patch_version = PatchVersion.objects.order_by("-version_str").first()
     item_list = patch_version.item_set.all()
 
     context = {
@@ -23,4 +20,21 @@ def index(request):
         'item_list': item_list,
     }
 
-    return render(request, 'gold_efficiency/index.html', context)
+    return render(request, 'gold_efficiency/itemlist.html', context)
+
+
+def itemdetail(request, item_id):
+    # TODO:インプットのヴァリデーション
+
+    item = Item.objects.get(pk=item_id)
+    input_params = dict()
+    for k, v in request.GET.items():
+        if v:
+            input_params[k] = v
+
+    context = {
+        'item': item,
+        'input': input_params,
+    }
+
+    return render(request, 'gold_efficiency/itemdetail.html', context)
