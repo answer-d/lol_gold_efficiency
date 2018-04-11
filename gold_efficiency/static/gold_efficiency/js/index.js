3276f03134da8d5a4446add2946e655cb40d4028
domahoki <nuko33@gmail.com>
2018-04-11 20:59:59 +0900

    ソートフィルターができました完

--- gold_efficiency/static/gold_efficiency/js/index.js
+++ gold_efficiency/static/gold_efficiency/js/index.js
@@ -1,6 +1,56 @@
-$('.box-container').isotope({
+var qsRegex;
+
+var $box = $('.box-container').isotope({
   itemSelector:'.box',
+  getSortData: {
+    name: '.item-name',
+    efficiency: function (elem) {
+      let eff = parseFloat($(elem).find('.efficiency').text().replace('%', ''));
+      return eff;
+    },
+    value: function (elem) {
+      let str = $(elem).find('.value').text().match(/\d+\.*\d*G/)[0];
+      return parseFloat(str.replace('G', ''));
+    },
+    cost: function (elem) {
+      let str = $(elem).find('.total').text().match(/\d*G/)[0];
+      return parseInt(str.replace('G', ''));
+    },
+  },
+  filter: function() {
+    return qsRegex ? $(this).text().match( qsRegex ) : true;
+  },
   masonry: {
     columnWidth: 100
   }
 });
+
+$('.btn-group').on('click', 'button', function(){
+  var sortValue = $(this).attr('sort-type');
+  console.log(sortValue);
+  $box.isotope({
+    sortBy: sortValue,
+    sortAscending: false,
+  });
+});
+
+// use value of search field to filter
+var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+  $box.isotope();
+}, 200 ) );
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  threshold = threshold || 100;
+  return function debounced() {
+    clearTimeout( timeout );
+    var args = arguments;
+    var _this = this;
+    function delayed() {
+      fn.apply( _this, args );
+    }
+    timeout = setTimeout( delayed, threshold );
+  };
+}
